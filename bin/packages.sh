dc7accee45d99d54f87bf9e265a04dc7c2d020d8
Technote <technote.space@gmail.com>
2019-12-30 23:35:39 +0900

    chore: tweaks

--- bin/packages.sh
+++ bin/packages.sh
@@ -7,6 +7,7 @@ rm -f "${WORKSPACE}/composer.lock"
 
 modules=$(< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.key | startswith("wp")) | .key' | tr '\n' ' ')
 if [[ -n "${modules}" ]]; then
-  composer require --working-dir="${WORKSPACE}" --no-interaction --prefer-dist --no-suggest "${modules}"
+  # shellcheck disable=SC2086
+  composer require --working-dir="${WORKSPACE}" --no-interaction --prefer-dist --no-suggest ${modules}
 fi
 composer require --dev --working-dir="${WORKSPACE}" --no-interaction --prefer-dist --no-suggest dealerdirect/phpcodesniffer-composer-installer phpmd/phpmd squizlabs/php_codesniffer wp-coding-standards/wpcs phpcompatibility/phpcompatibility-wp

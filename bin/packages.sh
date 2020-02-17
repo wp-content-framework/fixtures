7b4f15ef1574124a5c3fb021370a3e5263ed1724
Technote <technote.space@gmail.com>
2020-02-17 23:48:36 +0900

    chore: tweaks

--- bin/packages.sh
+++ bin/packages.sh
@@ -5,5 +5,8 @@ set -e
 rm -rdf "${WORKSPACE}/vendor"
 rm -f "${WORKSPACE}/composer.lock"
 
-< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.value | startswith("^") or startswith("~")) | select(.key | contains("/")) | .key' | tr '\n' ' ' | xargs -r php -d memory_limit=2G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
-< "${WORKSPACE}/composer.json" jq -r '."require-dev" | to_entries[] | select(.value | startswith("^") or startswith("~")) | select(.key | contains("/")) | .key' | tr '\n' ' ' | xargs -r php -d memory_limit=2G "$(command -v composer)" require --dev --no-interaction --prefer-dist --no-suggest
+#< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.value | startswith("^") or startswith("~")) | select(.key | contains("/")) | .key' | tr '\n' ' ' | xargs -r php -d memory_limit=2G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
+#< "${WORKSPACE}/composer.json" jq -r '."require-dev" | to_entries[] | select(.value | startswith("^") or startswith("~")) | select(.key | contains("/")) | .key' | tr '\n' ' ' | xargs -r php -d memory_limit=2G "$(command -v composer)" require --dev --no-interaction --prefer-dist --no-suggest
+
+< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key' | tr '\n' ' ' | xargs -r php -d memory_limit=2G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
+< "${WORKSPACE}/composer.json" jq -r '."require-dev" | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key' | tr '\n' ' ' | xargs -r php -d memory_limit=2G "$(command -v composer)" require --dev --no-interaction --prefer-dist --no-suggest

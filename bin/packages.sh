789e5e904cdb3184b80ddb2a2f6ebfe006948d31
Technote <technote.space@gmail.com>
2020-08-28 18:30:25 +0900

    chore: tweaks

--- bin/packages.sh
+++ bin/packages.sh
@@ -7,9 +7,9 @@ rm -f "${WORKSPACE}/composer.lock"
 
 TARGET=$(< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key')
 if [[ -n "${TARGET}" ]]; then
-  echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=2G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
+  echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=4G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
 fi
 TARGET=$(< "${WORKSPACE}/composer.json" jq -r '."require-dev" | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key')
 if [[ -n "${TARGET}" ]]; then
-  echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=2G "$(command -v composer)" require --dev --no-interaction --prefer-dist --no-suggest
+  echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=4G "$(command -v composer)" require --dev --no-interaction --prefer-dist --no-suggest
 fi

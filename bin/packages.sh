1ed3f39244132395997a9845c85005d3e8dcaaf3
Technote <technote.space@gmail.com>
2020-08-28 18:38:40 +0900

    chore: tweaks

--- bin/packages.sh
+++ bin/packages.sh
@@ -5,11 +5,11 @@ set -e
 rm -rdf "${WORKSPACE}/vendor"
 rm -f "${WORKSPACE}/composer.lock"
 
-TARGET=$(< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key')
-if [[ -n "${TARGET}" ]]; then
-  echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=4G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
-fi
 TARGET=$(< "${WORKSPACE}/composer.json" jq -r '."require-dev" | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key')
 if [[ -n "${TARGET}" ]]; then
   echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=4G "$(command -v composer)" require --dev --no-interaction --prefer-dist --no-suggest
+fi
+TARGET=$(< "${WORKSPACE}/composer.json" jq -r '.require | to_entries[] | select(.value | startswith("^")) | select(.key | contains("/")) | .key')
+if [[ -n "${TARGET}" ]]; then
+  echo "${TARGET}" | tr '\n' ' ' | xargs php -d memory_limit=4G "$(command -v composer)" require --no-interaction --prefer-dist --no-suggest
 fi
